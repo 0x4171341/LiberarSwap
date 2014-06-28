a094eb9a0d15440ff202eca08e0c2ed152b09e09
0x4171341 <alfierimorillo@gmail.com>
2014-06-28 02:05:14 -0430

    Create Liberar.sh

---
+++ Liberar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+zenity --hide-text --entry --title="digite la contraseña" | su - root -c "bash /home/x4171341/Escritorio/codigos/Limpiarcache.sh"
+zenity --progress \
+--title="Limpiando" \
+--text="Limpiando los registros..." \
+--percentage=0
+if [ "$?" = -1 ] ; then
+zenity --error \
+--text="cancelada."
+fi''
+if zenity --entry \
+--title="actualizar" \
+if  su -c; then
+
+sync ; echo 1 > /proc/sys/vm/drop_caches
+sync ; echo 2 > /proc/sys/vm/drop_caches
+sync ; echo 3 > /proc/sys/vm/drop_caches;
+
+[elif ; then
+
+comandos;]
